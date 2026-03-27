9a9b8671f7350a03eab76223839ddff958d28588
chancellor-whitaker <chancellor.whitaker@eku.edu>
2026-03-27 10:32:05 -0400

    first commit

---
+++ js/emojipediaNetworkDataToSourceUrls.js
@@ -0,0 +1,15 @@
+export default function emojipediaNetworkDataToSourceUrls(data) {
+  const urls = !data ? [] : data.log.entries.map((e) => e.request.url);
+
+  const webpUrls = urls.filter(
+    (url) =>
+      url.endsWith(".webp") &&
+      url.startsWith("https://em-content.zobj.net/thumbs/60"),
+  );
+
+  const pngUrls = webpUrls.map((url) =>
+    url.replace("thumbs/60", "source").replace(".webp", ".png"),
+  );
+
+  return [...new Set(pngUrls)];
+}
