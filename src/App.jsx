41a58f93b3d72bc59e313d6e6032a7ba4a5f57e5
chancellor-whitaker <chancellor.whitaker@eku.edu>
2026-03-27 12:42:28 -0400

    getting emoji urls

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import { cdn } from "https://cdn.jsdelivr.net/gh/chancellor-whitaker/github-cdn@master/static/my-lib.js";
+import { js } from "https://cdn.jsdelivr.net/gh/chancellor-whitaker/github-cdn@master/static/my-lib.js";
+import useData from "./hooks/useData";
 
-console.log(cdn);
+// console.log(js);
 
 export default function App() {
+  const data = useData("emojipedia.org.json");
+
+  const urls = js.functions.convertEmojipediaNetworkDataToSourceUrls(data);
+
+  console.log(urls);
+
   return <></>;
 }
